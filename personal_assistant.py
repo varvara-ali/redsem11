f39ca29e9ff37f11ab5a3b69d638a2d08d9f65bd
varvara-ali <varvara.alimova@gmail.com>
2024-12-01 20:35:55 +0300

    note

---
+++ personal_assistant.py
@@ -0,0 +1,14 @@
+import json
+from json.decoder import JSONDecodeError
+from pprint import pprint
+from typing import TypedDict
+import csv
+from datetime import datetime
+from fnmatch import fnmatch
+
+
+class Note(TypedDict):
+    id: int
+    title: str
+    content: str
+    timestamp: str
